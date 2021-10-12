8abaf561ffa9587381fb2484b0413e4d131188ee
natalialbraz <natalialbraz@gmail.com>
2021-10-12 11:46:50 -0300

    Aula de RealTime Database - falta implementar o campo email

--- app/src/main/java/com/example/aulasfirebase/HomeActivity.kt
+++ app/src/main/java/com/example/aulasfirebase/HomeActivity.kt
@@ -3,11 +3,24 @@ package com.example.aulasfirebase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.aulasfirebase.databinding.ActivityHomeBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.config.GservicesValue.value
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+
+/*data class Info(
+    val etName: String? = null,
+    val etEmail: String? = null
+        ) {}*/
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -20,6 +33,41 @@ class HomeActivity : AppCompatActivity() {
             Firebase.auth.signOut()
             logOut()
         }
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message").child("config")
+
+        val etName = binding.etName
+        val etEmail = binding.etEmail
+        val btnSave = binding.btSave
+        val tvNameAtual = binding.tvNameLoad
+        val tvEmailAtual = binding.tvEmailLoad
+
+
+        btnSave.setOnClickListener {
+            myRef.child("config").setValue(etName.text.toString())
+            Toast.makeText(this@HomeActivity, "Configurações Salvas", Toast.LENGTH_SHORT).show()
+        }
+
+
+        // Read from the database
+        myRef.child("config").addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                tvNameAtual.text = value
+                //tvEmailAtual.text = value?.etEmail
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@HomeActivity, "Ocorreu um erro", Toast.LENGTH_SHORT).show()
+                Log.w("TAG", "Failed to read value.", error.toException())
+            }
+
+        })
+
     }
     private fun logOut(){
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -32,4 +80,5 @@ class HomeActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
 }
